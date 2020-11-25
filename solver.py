b2dc73d87177793a43e936005a6b9fdb910337c9
Bill Huang <billhuang0916@gmail.com>
2020-11-25 00:12:41 -0600

    Generate Random Inputs

--- solver.py
+++ solver.py
@@ -16,60 +16,7 @@ def solve(G, s):
 
     #TODO: your code here!
     return bruteForce(G, s)
-
-
-def bruteForce(G, s):
-    numStudents = len(G.nodes)
-
-    bestAssignment = None
-    bestNumRooms = -1
-    bestScore = -1
-
-    curAssignment = {}
-
-    def dfs(curAssignment, numAssigned, numRooms):
-        nonlocal bestAssignment, bestNumRooms, bestScore
-        # Ignore assignments with more than "numStudents" rooms.
-        if numRooms >= numStudents:
-            return
-
-        # If all students are assigned check if assignment is better
-        if numAssigned >= numStudents:
-            curScore = calculate_happiness(curAssignment, G)
-            if curScore > bestScore:
-                bestAssignment = curAssignment.copy()
-                bestNumRooms = numRooms
-                bestScore = curScore
-            return
-
-        # Go through all possible room assignments for the next student
-        for room in range(numRooms + 1):
-            curAssignment[numAssigned] = room
-            newNumRooms = max(numRooms, room + 1)
-
-            # # Check if assignment is valid so far
-            # roomAssignments = {}
-            # for i in range(numAssigned + 1):
-            #     curRoom = curAssignment[i]
-            #     if curRoom not in roomAssignments:
-            #         roomAssignments[curRoom] = [i]
-            #     else:
-            #         roomAssignments[curRoom].append(i)
-            #
-            # maxRoomStress = s / newNumRooms
-            # isValid = True
-            # print(roomAssignments)
-            # for breakoutRoom in roomAssignments:
-            #     isValid &= maxRoomStress < calculate_stress_for_room(breakoutRoom, G)
-
-            if is_valid_solution(curAssignment, G, s, newNumRooms):
-                dfs(curAssignment, numAssigned + 1, newNumRooms)
-
-        del curAssignment[numAssigned]
-
-    dfs(curAssignment, 0, 0)
-    return bestAssignment, bestNumRooms
-
+    
 # Here's an example of how to run your solver.
 
 # Usage: python3 solver.py test.in
