1309382a33e4630fac2c0561a0d63f0adcedab01
Bill Huang <billhuang0916@gmail.com>
2020-11-25 01:05:46 -0600

    Add Timing

--- solver.py
+++ solver.py
@@ -1,7 +1,7 @@
 import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import is_valid_solution, calculate_happiness, calculate_stress_for_room, convert_dictionary
-import sys
+import sys, time
 from bruteforce import bruteForce
 from estimator import estimate
 
@@ -27,10 +27,18 @@ if __name__ == '__main__':
     assert len(sys.argv) == 2
     path = sys.argv[1]
     G, s = read_input_file(path)
+
+    start = time.time()
     D, k = solve(G, s)
+    end = time.time()
+
     assert is_valid_solution(D, G, s, k)
     print("Total Happiness: {}".format(calculate_happiness(D, G)))
-    write_output_file(D, f'test/{path[8:-3]}.out')
+    print("Solving took {} seconds.".format(end - start))
+    if path[-3:] == ".in":
+        write_output_file(D, f'{path[:-3]}.out')
+    else:
+        write_output_file(D, f'test/test.out')
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
