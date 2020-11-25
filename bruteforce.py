6870fab0920ed1dc521c5f8c37cbce1b5ae7552a
Bill Huang <billhuang0916@gmail.com>
2020-11-25 01:38:48 -0600

    Add Timeout to Bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -1,19 +1,38 @@
 import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import is_valid_solution, calculate_happiness, calculate_stress_for_room
-import sys
+import time
 
 def bruteForce(G, s):
+    bruteForce(G, s, 0)
+
+
+def bruteForce(G, s, timeoutInSeconds):
+    """
+    Brute force with backtracking on solution validity
+    Args:
+        G: Graph representation of the problem
+        s: Max Stress
+        timeoutInSeconds: The amount of time to run the algorithm.
+        0 if you want no timeout
+
+    Returns: Best solution sound so far
+
+    """
     numStudents = len(G.nodes)
 
     bestAssignment = None
     bestNumRooms = -1
     bestScore = -1
-
+    start = time.time()
     curAssignment = {}
 
     def dfs(curAssignment, numAssigned, numRooms):
         nonlocal bestAssignment, bestNumRooms, bestScore
+        # End early if timed out and return best solution
+        if timeoutInSeconds and time.time() > start + timeoutInSeconds:
+            return
+
         # Ignore assignments with more than "numStudents" rooms.
         if numRooms >= numStudents:
             return
